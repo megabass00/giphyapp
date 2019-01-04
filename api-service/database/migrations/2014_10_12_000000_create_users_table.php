8f715bbe5cb78c3fa08da2cbd0f9f5ffe52c00c5
Luis Izquierdo <luisma@jig.es>
2019-01-04 08:57:56 +0100

    Añadido campo image a la tabla users

--- api-service/database/migrations/2014_10_12_000000_create_users_table.php
+++ api-service/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->enum('type', ['admin','member'])->default("member");
             $table->string('name');
             $table->string('last_name');
+            $table->string('image');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
